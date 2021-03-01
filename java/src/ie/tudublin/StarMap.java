bc6bcf3573c12e7d9e677575d738a348be7ebead
Dansti <danstibayev@gmail.com>
2021-03-01 13:21:37 +0000

    Attempt 1

--- java/src/ie/tudublin/StarMap.java
+++ java/src/ie/tudublin/StarMap.java
@@ -1,8 +1,24 @@
 package ie.tudublin;
 
+import java.util.ArrayList;
 import processing.core.PApplet;
+import processing.data.Table;
+import processing.data.TableRow;
 
 public class StarMap extends PApplet {
+
+    ArrayList<Star> stars = new ArrayList<Star>();
+
+    void loadStars()
+    {
+        Table table = loadTable("HabHYG15ly.csv", "header");
+        for(TableRow row:table.rows())
+        {
+            Star s = new Star(row);
+            stars.add(s);
+        }
+    }
+
     public void settings() {
         size(500, 500);
     }
@@ -14,6 +30,7 @@ public class StarMap extends PApplet {
 
     public void setup() {
         colorMode(RGB);
+        loadStars();
     }
 
     public void draw() {
